779f47d86cec96b9cb52e1c6058b9162d63ba65a
Tiwill1818 <169067706+Tiwill1818@users.noreply.github.com>
2024-09-23 08:58:54 -0400

    Atelier10V1

---
+++ atelier6.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var app = express();
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+    //res.setHeader('Content-Type', 'text/html');
+    res.render('./pages/accueil');
+});
+
+app.get('/contact', function (req, res) {
+    //res.setHeader('Content-Type', 'text/html');
+    res.render('./pages/contact');
+});
+app.get('/about', function (req, res) {
+    //res.setHeader('Content-Type', 'text/html');
+    res.render('./pages/about');
+    
+});
+app.get('/recherche', function (req, res) {
+    //res.setHeader('Content-Type', 'text/html');
+    //res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8'});
+    res.render('./pages/recherche', {mois: req.query.mois, annee: req.query.annee});
+});
+app.use(function (req, res, next) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(404).send('Page introuvable !');
+});
+app.listen(8080);
+
+
+
+/*var server = http.createServer(function (req, res) {
+    var page = url.parse(req.url).pathname;
+    var param = url.parse(req.url, true).query;
+    console.log(page);
+    
+    if (page == '/') {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end('<h1> Bienvenue dans mon super site ! </h1>');
+    }
+    else if (page == '/contact') {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write('<head><meta charset="UTF-8"></head>');
+        res.write('<p>Me contacter : <a href="mailto:billgates@hotmail.com">billgates@hotmail.com</a></p>');
+        res.end('<br/><a href="/">Retour à l\'accueil</a>');
+    }
+    else if (page == '/recherche' && param.annee && param.mois) {
+        res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+        res.end('Vous êtes dans la section recherche. Voici les résultats pour la date de '+param.mois+' '+param.annee);
+    }
+    else {
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end('ERREUR 404');
+    }
+    
+});
+server.listen(8080);*/
